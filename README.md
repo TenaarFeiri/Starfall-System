81ef120bd4e1b1b4da09c8b55ba2e4cdfeb33553
TenaarFeiri <tenaarfeiri@gmail.com>
2023-05-25 14:16:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Starfall System
+ 
---
+++ attachment_controller.php
@@ -0,0 +1,10 @@
+<?php
+
+    // Get the headers sent from the Second Life client.
+    $headers = getallheaders();
+    
+    // Then we set up our debug constant.
+    const DEBUG = true; // False for production, true for development.
+
+
+?>
